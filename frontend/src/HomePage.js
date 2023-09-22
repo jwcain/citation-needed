c20d38b9d6e401dce53fc8a2c5786e313fd8d03b
Justin Cain <justinwcain@gmail.com>
2023-09-22 03:37:51 -0400

    created component for bounded text entry field

--- frontend/src/HomePage.js
+++ frontend/src/HomePage.js
@@ -1,5 +1,82 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
 
 export default function HomePage() {
-  return <div>Homepage</div>;
+  let [textValue, setTextValue] = useState("");
+
+  return (
+    <div>
+      <BoundedTextEntry
+        onSubmit={(text) => {
+          setTextValue(text);
+        }}
+        defaultValue={textValue}
+        lowerBound={4}
+        upperBound={16}
+      />
+      <p>{textValue}</p>
+    </div>
+  );
+}
+
+function BoundedTextEntry({ onSubmit, defaultValue, lowerBound, upperBound }) {
+  let [lastUsedValue, setLastUsedValue] = useState(defaultValue);
+  let bounded =
+    lastUsedValue.length >= lowerBound && lastUsedValue.length <= upperBound;
+  function onInputChange(event) {
+    setLastUsedValue(event.target.value);
+    bounded =
+      lastUsedValue.length >= lowerBound && lastUsedValue.length <= upperBound;
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        defaultValue={defaultValue}
+        onChange={onInputChange}
+      />
+      <input
+        type="submit"
+        value="Enter"
+        onClick={() => {
+          if (bounded === false) return;
+          onSubmit(lastUsedValue);
+        }}
+      />
+      {bounded ? (
+        <></>
+      ) : (
+        <p>
+          Entry must be between {lowerBound} and {upperBound} characters.
+        </p>
+      )}
+    </div>
+  );
+}
+
+function BasicTextEntry({ onSubmit, defaultValue }) {
+  let lastUsedValue = defaultValue;
+  function onInputChange(event) {
+    lastUsedValue = event.target.value;
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        defaultValue={defaultValue}
+        onChange={onInputChange}
+      />
+      <input
+        type="submit"
+        value="Enter"
+        onClick={() => {
+          onSubmit(lastUsedValue);
+        }}
+      />
+      {}
+    </div>
+  );
 }
