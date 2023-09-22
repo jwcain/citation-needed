02f82c2a9d2c8d2ac24696c4e6d3423d85ce40bc
Justin Cain <justinwcain@gmail.com>
2023-09-22 00:17:08 -0400

    setup basic routing and layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage.js";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="d-flex flex-column min-vh-100">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
-export default App;
+function Main() {
+  return (
+    <main>
+      <Container className="mt-3">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Container>
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand>Citation Needed</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto w-100 justify-content-end">
+              <a href="/admin" className="nav-link" target="_blank">
+                Admin
+              </a>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+function Footer() {
+  return (
+    <footer className="mt-auto">
+      <div className="text-center">In Development. All Rights Reserved.</div>
+    </footer>
+  );
+}
---
+++ frontend/src/HomePage.js
@@ -0,0 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function HomePage() {
+  return <div>Homepage</div>;
+}
